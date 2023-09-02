246767fdc89947b5dc5261d6ddc64615b4ee0eb6
LyzunNataliia <ngovorushkina@gmail.com>
2023-09-02 15:09:03 +0300

    first-task-js-1

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,26 +2,93 @@ let nowYear = 2023;
 let childYear = 2008;
 let limetYear = 1900;
 
-let checkYear = prompt("Введіть рік для перевірки ", "");
+// let checkYear = prompt("Введіть рік для перевірки ", "");
 
-if (checkYear > nowYear) {
-  alert("Такий рік буде пізніше");
-} else if (checkYear < limetYear) {
-  alert("Стільки не живуть");
-} else if (checkYear == nowYear) {
-  alert("Новонароджений");
-} else if (checkYear >= childYear) {
-  alert("Період дитинства");
-} else {
-  alert("Eспішна регістрація");
+// switch (true) {
+//   case checkYear > nowYear: {
+//     alert("Такий рік буде пізніше");
+//     break;
+//   }
+//   case checkYear < limetYear: {
+//     alert("Стільки не живуть");
+//     break;
+//   }
+//   case checkYear == nowYear: {
+//     alert("Новонароджений");
+//     break;
+//   }
+//   case checkYear >= childYear: {
+//     alert("Період дитинства");
+//     break;
+//   }
+//   default: {
+//     alert("Успішна регістрація");
+//     break;
+//   }
+// }
+
+// checkYear = prompt("який зараз рік?", "");
+
+// let year = checkYear == nowYear;
+// let answer = year ? "Все вірно 2023p" : "Не вірно";
+// alert(answer);
+
+const form = document.querySelector(".form-color");
+const colorInput = document.querySelector("#color");
+const caseResult = document.querySelector("#caseResult");
+const colorDescripton = document.querySelector(".color-descripton");
+
+// part 1
+// function getValue() {
+//   const getColor = colorInput.Value;
+//   colorDescripton.innerHTML = getColor;
+// }
+// colorInput.addEventListener("input", getValue)
+
+// part 2
+// function getValue() {
+//   const getColor = colorInput.Value;
+//   colorDescripton.innerHTML = getColor;
+// }
+
+// caseResult.addEventListener("click", function (event) {
+//   event.preventDefault();
+//   getValue();
+// });
+
+// part 3
+function getValue() {
+  let pushColor;
+  const getColor = colorInput.value.toLowerCase();
+
+  switch (getColor) {
+    case "синій": {
+      pushColor = "Синій колір моря, можливо спокійний чи бурхливий";
+      colorDescripton.style.backgroundColor = "#AA0000";
+      break;
+    }
+    case "червоний": {
+      pushColor = "Колір уваги, зажди привертай увагу";
+      break;
+    }
+    case "жовтий": {
+      pushColor = "Теплий затишний колір - Жовтий";
+      break;
+    }
+    case "білий": {
+      pushColor = "Ще не визначено, що подобається";
+      break;
+    }
+    default: {
+      pushColor = "колір не визначено в переліку";
+      break;
+    }
+  }
+
+  colorDescripton.innerHTML = pushColor;
 }
 
-checkYear = prompt("який зараз рік?", "");
-
-if (checkYear < nowYear) {
-  alert("ні, трішки більше");
-} else if (checkYear > nowYear) {
-  alert("ні, трішки менше");
-} else {
-  alert("Все вірно, зараз = " + checkYear + " рік.");
-}
+caseResult.addEventListener("click", function (event) {
+  event.preventDefault();
+  getValue();
+});
